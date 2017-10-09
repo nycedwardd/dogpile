c7df048dcb06e748c339bd9e80efcc7fb9b35c2b
nycedwardd <ed@dembowski.net>
2017-10-09 08:42:19 +0200

    Updated reader.py

--- reader.py
+++ reader.py
@@ -1,44 +1,48 @@
-#import fileinput
-import investigate
-import json
+#!/usr/bin/python
+
+"""Read in data from a csv file and store enriched results within ElasticSearch"""
+# import json
 import csv
 import time
-from geoip import geolite2
-from elasticsearch import Elasticsearch
-from datetime import datetime
-es = Elasticsearch()
 
-infile = open('EXAMPLE.csv')
-reader = csv.DictReader(infile)
+#import fileinput
+import investigate
+from elasticsearch import Elasticsearch
+
+# from datetime import datetime
+ES = Elasticsearch()
+
+INFILE = open('EXAMPLE.csv')
+READER = csv.DictReader(INFILE)
 
 # ignore 400 cause by IndexAlreadyExistsException when creating an index
 #es.indices.create(index='csvdata', ignore=400)
-es.indices.create(index='csvdata')
+ES.indices.create(index='csvdata')
 # Read key, single line
 with open('api-key.txt', 'r') as k:
-    api_key = k.read().rstrip()
-inv = investigate.Investigate(api_key)
+    API_KEY = k.read().rstrip()
+INV = investigate.Investigate(API_KEY)
 
 # Roll through
-for row in reader:
+for row in READER:
     if row['cs_host'] != '':
         # printing row for diagnostic purposes so I know that script hasn't died
         print 'cs_host = ', row['cs_host']
-        row['investigate'] = inv.security(row['cs_host'])
-        row['domain_tags'] = inv.domain_tags(row['cs_host'])
-        row['rr_dns_history'] = inv.rr_history(row['cs_host'])
-        row['rr_ip_history'] = inv.rr_history(row['dst'])
-        tmpcat = inv.categorization(row['cs_host'], labels=True)
+        row['investigate'] = INV.security(row['cs_host'])
+        row['domain_tags'] = INV.domain_tags(row['cs_host'])
+        row['rr_dns_history'] = INV.rr_history(row['cs_host'])
+        row['rr_ip_history'] = INV.rr_history(row['dst'])
+        tmpcat = INV.categorization(row['cs_host'], labels=True)
         row['categorization'] = {}
         row['categorization']['security_status'] = tmpcat[row['cs_host']]['status']
         row['categorization']['security_categories'] = tmpcat[row['cs_host']
-                                                              ]['security_categories']
+                                                             ]['security_categories']
         row['categorization']['content_categories'] = tmpcat[row['cs_host']
-                                                             ]['content_categories']
-        row['country'] = geolite2.lookup(row['dst']).country
+                                                            ]['content_categories']
+        # row['country'] = geolite2.lookup(row['dst']).country
         # cshost = {}
         # cshost['score'] =
     else:
         pass
     time.sleep(50.0 / 1000.0)
-    es.index(index='csvdata', doc_type='EXAMPLE', body=row)
+    ES.index(index='csvdata', doc_type='EXAMPLE', body=row)
